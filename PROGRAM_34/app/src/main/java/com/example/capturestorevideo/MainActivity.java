18966d3ed725d66bf490e2b2e312a76bea69809f
Abhi Maru <72540047+Abhimaru@users.noreply.github.com>
2023-09-19 13:09:36 +0530

    PROGRAM 34 UPDATED

--- PROGRAM_34/app/src/main/java/com/example/capturestorevideo/MainActivity.java
+++ PROGRAM_34/app/src/main/java/com/example/capturestorevideo/MainActivity.java
@@ -1,23 +1,49 @@
 package com.example.capturestorevideo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
     Button btnCaptureVideo;
+    VideoView videoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnCaptureVideo = findViewById(R.id.btnCapture_p34);
+        videoView = findViewById(R.id.videoView_p34);
 
         btnCaptureVideo.setOnClickListener(v -> {
             Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-            startActivity(intent);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(this, "No video capture app available", Toast.LENGTH_SHORT).show();
+            }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST) {
+            assert data != null;
+            Uri videoUri = data.getData();
+            videoView.setVisibility(View.VISIBLE);
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+            Toast.makeText(this, "Video saved to Device", Toast.LENGTH_LONG).show();
+        }
     }
 }
